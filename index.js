867708a9bd7f2170b3f59a79332539d57849937d
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-12-03 11:48:10 +0300

    animated nav bar

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 gsap.to(".one", {delay: 1, duration: 2, x: -400, ease: "power2.inOut", opacity: 0})
 gsap.from(".two", {delay: 1.2, duration: 2, x: - 400, ease: "power2.inOut", opacity: 0})
+gsap.from(".header", {delay: 3, duration: 2, y: 90, opacity: 0})
+
