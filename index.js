4c0e222e5ac93c4770cd953e5ad9987db122d222
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-12-03 11:54:20 +0300

    animation for button in JS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 gsap.to(".one", {delay: 1, duration: 2, x: -400, ease: "power2.inOut", opacity: 0})
 gsap.from(".two", {delay: 1.2, duration: 2, x: - 400, ease: "power2.inOut", opacity: 0})
-gsap.from(".header", {delay: 3, duration: 2, y: 90, opacity: 0})
+gsap.from(".header", {delay: 3, duration: 2, y: 80, opacity: 0})
+gsap.from(".btn", {opacity: 0, delay: 4, duration: 1, rotation: 720})
 
