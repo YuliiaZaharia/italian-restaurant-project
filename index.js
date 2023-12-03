1be893d0260e6a4547109deac91393e359a63324
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-12-03 12:00:09 +0300

    last touches

--- index.js
+++ index.js
@@ -6,5 +6,4 @@ gsap.from(".btn", {opacity: 0, delay: 4, duration: 1, rotation: 720})
 gsap.from('.mobile', { duration: 2, delay: 2.4, y: '10vw', ease: 'power2.in', opacity: 0 })
 gsap.from('h1', { duration: 2, delay: 1.5, x: '-100vw', ease: 'power2.in', opacity: 0 })
 gsap.fromTo('.btn', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, delay: 4.3, opacity: 1, scale: 1, rotation: 0 })
-gsap.from('.links', { duration: 2, delay: 2.4, y: '10vw', ease: 'power2.in', opacity: 0 })
 
